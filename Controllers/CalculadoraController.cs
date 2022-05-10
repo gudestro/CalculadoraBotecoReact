fbfe1060aaefffd81a0cbf7eae218595afcb36c6
Gustavo Destro <92490380+gudestro@users.noreply.github.com>
2022-05-09 22:14:26 -0300

    Add files via upload

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,15 @@
+﻿using Calculadora_de_Buteco.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculadora_de_Buteco.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Calculadora_de_Buteco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
